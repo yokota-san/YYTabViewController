bea6aa247ea00cf7f3689a2e98bcb11d6adf3bce
yokota-san <yokota-san@users.noreply.github.com>
2015-04-26 15:59:20 +0900

    startup files

---
+++ YYSwiftTabViewController/YYSwiftTabViewController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YYSwiftTabViewController
+//
+//  Created by yokotasan on 2015/04/26.
+//  Copyright (c) 2015年 yokotasan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ YYTabVIewController/YYTabVIewController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YYTabVIewController
+//
+//  Created by yokotasan on 2015/04/26.
+//  Copyright (c) 2015年 yokotasan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
